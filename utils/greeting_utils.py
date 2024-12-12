0a36a7addc8d095b1cd80bbe301b69a8349312f0
erdenebulgan1011 <126801859+erdenebulgan1011@users.noreply.github.com>
2024-12-12 15:30:59 +0800

    Create greeting_utils.py

---
+++ utils/greeting_utils.py
@@ -0,0 +1,16 @@
+def hello():
+    """Print a hello message."""
+    print("Hi there!")
+
+def bye():
+    """Print a goodbye message."""
+    print("Goodbye!")
+
+def welcome(name):
+    """
+    Print a personalized welcome message.
+    
+    Args:
+        name (str): The name of the person to welcome.
+    """
+    print(f"Welcome, {name}! Nice to meet you.")
